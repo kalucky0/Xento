c9906bfa6d8a45b612e77ef24235b9de2be57330
kalucky0 <kalucki23@gmail.com>
2023-04-18 17:00:06 +0200

    Add method enums to PNG lib

---
+++ userland/libs/png/src/enums.rs
@@ -0,0 +1,20 @@
+pub enum CompressionMethod {
+    Deflate,
+}
+
+pub enum FilterMethod {
+    Adaptive,
+}
+
+pub enum FilterType {
+    None,
+    Sub,
+    Up,
+    Average,
+    Paeth,
+}
+
+pub enum InterlaceMethod {
+    None,
+    Adam7,
+}
--- userland/libs/png/src/lib.rs
+++ userland/libs/png/src/lib.rs
@@ -1,18 +1,19 @@
 mod ancillary_chunks;
 mod bit_depth;
-mod chunk_type;
 mod chunk;
+mod chunk_type;
 mod color_type;
+mod enums;
 mod pixel_type;
 mod scanline_iter;
 mod transparency_chunk;
 
 pub use ancillary_chunks::AncillaryChunks;
 pub use bit_depth::BitDepth;
-pub use chunk_type::ChunkType;
 pub use chunk::Chunk;
+pub use chunk_type::ChunkType;
 pub use color_type::ColorType;
+pub use enums::{CompressionMethod, FilterMethod, FilterType, InterlaceMethod};
 pub use pixel_type::PixelType;
+pub use scanline_iter::ScanlineIterator;
 pub use transparency_chunk::TransparencyChunk;
-
-// https://www.w3.org/TR/PNG-Chunks.html
